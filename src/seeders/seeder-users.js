0f2deca5ad35a05dfd6e767f638a691af9a7d074
vanduoc <vanduoc198@gmail.com>
2022-11-15 20:23:33 +0700

    create all models, migrations and seeders

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -5,9 +5,14 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Users', [
             {
-                firstName: 'John',
-                lastName: 'Doe',
-                email: 'example@example.com',
+                email: 'admin@gmail.com',
+                password: '123456', //plain text --> sfdsfasdfsdgf12234 : hash password
+                firstName: 'Duoc',
+                lastName: 'Huynh van',
+                address: 'Ho Chi Minh',
+                gender: 1,
+                typeRole: 'ROLE',
+                keyRole: 'R1',
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
