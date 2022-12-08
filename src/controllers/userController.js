754ad792320a21f15b12e832c85825bf6e5e2b8c
vanduoc <vanduoc198@gmail.com>
2022-12-08 23:46:31 +0700

    change body to query

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ let handleLogin = async (req, res) => {
 };
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.body.id; //ALL, ID
+    let id = req.query.id; //ALL, ID
     if (!id) {
         return res.status(200).json({
             errCode: 0,
