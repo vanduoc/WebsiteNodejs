10b54fdd86e86ec74f2ee309d26bbc81d790c9b1
vanduoc <vanduoc198@gmail.com>
2022-12-10 18:58:54 +0700

    fixbug create new user

--- src/services/userService.js
+++ src/services/userService.js
@@ -105,24 +105,25 @@ let createNewUser = async (data) => {
                     errCode: 1,
                     message: 'Your email is already exist, plz try another email!!!',
                 });
+            } else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    image: data.image,
+                    roleId: data.roleId,
+                    positionId: data.positionId,
+                });
+                resolve({
+                    errCode: 0,
+                    message: 'OK',
+                });
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender === '1' ? true : false,
-                image: data.image,
-                roleId: data.roleId,
-                positionId: data.positionId,
-            });
-            resolve({
-                errCode: 0,
-                message: 'OK',
-            });
         } catch (error) {
             reject(error);
         }
